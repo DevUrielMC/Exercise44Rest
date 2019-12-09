c439ce1e3ac8711553e0d5303c250e7717d7d460
DevUrielMC <urimarcervantes.95@gmail.com>
2019-12-09 14:02:19 -0600

    Segundo commit añadiendo un nuevo package de servicios y una nueva clase con Path y Metodos GET Y POST

--- src/com/exercise44rest/services/Mensajes.java
+++ src/com/exercise44rest/services/Mensajes.java
@@ -1,15 +1,23 @@
 package com.exercise44rest.services;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 @Path("/msgs")
 public class Mensajes {
 
 	
-	@GET
+	@GET									//Si se quiere acceder por la barra de direcciones tiene que ser GET
+											//Con el metodo POST no es posible acceder
 	public String diHola() 
 	{
-		return "HOLA MUNDO";
+		return "HOLA MUNDO DESDE EL GET";
+	}
+	
+	@POST
+	public String diHolaMundoMsg()
+	{
+		return "HOLA MUNDO DESDE EL POST";
 	}
 }
